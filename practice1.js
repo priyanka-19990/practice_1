8d9054e8b995a118285fb1cdcd0f6178c28e5a1d
Priyanka <priyanka@cybergindia.local>
2022-05-12 10:07:23 +0530

    Commit by Priyanka

--- practice1.js
+++ practice1.js
@@ -1,64 +1,164 @@
-const form = document.getElementById('form');
-const username = document.getElementById('username');
-const email = document.getElementById('email');
-const password = document.getElementById('password');
-const password_check = document.getElementById('password_check');
+// First Name Validation
+var fValid = document.getElementById("fName");
 
-form.addEventListener('submit', e => {
-        e.preventDefault();
 
-        checkInputs();
-});
+var firstNameValidation = function () {
+  let firstNameValue = fValid.value.trim();
+  let validFirstName = /^[A-Za-z]+$/;
+  let firstNameErr = document.getElementById("firstNametag");
+    firstNameErr.style.color = "red";
 
-function checkInputs(){
-    const usernameValue = username.value.trim();
-    const emailValue = email.value.trim();
-    const passwordValue = password.value.trim();
-    const password_checkValue = password_check.value.trim();
+  if (firstNameValue == "") {
+    firstNameErr.innerHTML = "First Name is required";
+  } else if (!validFirstName.test(firstNameValue)) {
+    firstNameErr.innerHTML =
+      "First Name must be only string without white spaces";
+        
+  } else {
+    firstNameErr.innerHTML = "";
+    return true;
+  }
+};
 
-    if(usernameValue === ''){
-        //show error
-        //add error class
-        setErrorFor(username,'Please fill the field');
-    }else{
-        //add success class
-        setSuccessFor(username);
-    }
-    if(emailValue === ''){
-        setErrorFor(email,'Please fill the field');
-    }else if(!isEmail(emailValue)){
-        setErrorFor(email,'Email is not valid');
-    }else{
-        setSuccessFor(email);
-    }
-    if(passwordValue === ''){
-        setErrorFor(password,'Please fill the field');
-    }else{
-        setSuccessFor(password);
-    }
-    if(password_checkValue === ''){
-        setErrorFor(password_check,'Please fill the field');
-    }else if(passwordValue !== password_checkValue){
-        setErrorFor(password_check,'Password does not match');
-    }else{
-        setSuccessFor(password_check);
-    }
+fName.oninput = function () {
+  firstNameValidation();
+};
 
+// Last Name Validations
+var lvalid = document.getElementById("lName");
+
+var lastNameValidation = function () {
+  lastNameValue = lvalid.value.trim();
+  validLastName = /^[A-Za-z]+$/;
+  lastNameErr = document.getElementById("lasttNametag");
+  lastNameErr.style.color = "red";
+
+  if (lastNameValue == "") {
+    lastNameErr.innerHTML = "Last Name is required";
+  } else if (!validLastName.test(lastNameValue)) {
+    lastNameErr.innerHTML =
+      "Last Name must be only string without white spaces";
+  } else {
+    lastNameErr.innerHTML = "";
+    return true;
+  }
+};
+
+lName.oninput = function () {
+  lastNameValidation();
+};
+
+// Email Address Validation
+var emailAddress = document.getElementById("mail");
+var emailAddressValidation = function () {
+  emailAddressValue = emailAddress.value.trim();
+  validEmailAddress = /^\w+([\.-]?\w+)@\w+([\.-]?\w+)(\.\w{2,3})+$/;
+  emailAddressErr = document.getElementById("emailNametag");
+  emailAddressErr.style.color = "red";
+  if (emailAddressValue == "") {
+    emailAddressErr.innerHTML = "Email Address is required";
+  } else if (!validEmailAddress.test(emailAddressValue)) {
+    emailAddressErr.innerHTML =
+      "Email Address must be in valid formate with @ symbol";
+  } else {
+    emailAddressErr.innerHTML = "";
+    return true;
+  }
+};
+
+mail.oninput = function () {
+  emailAddressValidation();
+};
+
+// Mobile Number Validation
+var mobileNumber = document.getElementById("phone");
+
+var mobileNumberValidation = function () {
+  mobileNumberValue = mobileNumber.value.trim();
+  validMobileNumber = /^[0-9]*$/;
+  mobileNumberErr = document.getElementById("mobileTag");
+    mobileNumberErr.style.color="red";
+
+
+  if (mobileNumberValue == "") {
+    mobileNumberErr.innerHTML = "Mobile Number is required";
+  } 
+  
+  
+  
+  else if (mobileNumberValue.length != 10) {
+    mobileNumberErr.innerHTML = "Mobile Number must have 10 digits";
+  } else {
+    mobileNumberErr.innerHTML = "";
+    return true;
+  }
+};
+phone.oninput = function () {
+  mobileNumberValidation();
+};
+
+
+
+
+// Password Validation
+var passwordvalid= document.getElementById("formpass");
+passwordvalid.setAttribute('type' , 'password');
+
+var passwordValidation = function(){
+
+  passwordValue=passwordvalid.value.trim(); 
+   validPassword=/^(?=.[a-z])(?=.[A-Z])(?=.\d)(?=.[@$!%?&])[A-Za-z\d@$!%?&]{8,}$/
+   
+   passwordErr = document.getElementById("passwordtag");
+
+   passwordErr.style.color="red";
+
+   if(passwordValue=="")
+   {
+    passwordErr.innerHTML="Password is required";
+   }else if(!validPassword.test(passwordValue)){
+     passwordErr.innerHTML="Password must have at least one Uppercase, lowercase, digit, special characters & 8 characters";
+   }
+   else{
+     passwordErr.innerHTML="";
+     return true;
+   }
 }
-function setErrorFor(input, message){
-    const formControl = input.parentElement; //.form-control
-    const small = formControl.querySelector('small');
 
-    //add error message inside small
+passwordvalid.oninput=function(){
 
-    // add error class
-    formControl.className = 'form-control error';
-    small.innerText = message;
+   passwordValidation();
+
+//  confirmPasswordValidation();
+   
 }
-function setSuccessFor(input){
-    const formControl = input.parentElement; 
-    formControl.className = 'form-control success';
+
+// Confirm Password Validation
+var confirmPassword= document.getElementById("confirmpass");
+
+confirmPassword.setAttribute("type", "password");
+
+var confirmPasswordValidation=function(){
+   confirmPasswordValue=confirmPassword.value.trim(); 
+   
+   confirmPasswordErr=document.getElementById('confirmpasswordtag');
+
+   confirmPasswordErr.style.color="red";
+   if(confirmPasswordValue==""){
+       confirmPasswordErr.innerHTML="Confirm Password is required";
+   }
+
+  else if(confirmPasswordValue!=passwordvalid.value){
+     confirmPasswordErr.innerHTML="Password Mismatch";
+   }
+   else{
+     confirmPasswordErr.innerHTML="";
+     return true;
+   }
 }
-function isEmail(email){
-    return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+
+confirmPassword.oninput=function(){
+
+ confirmPasswordValidation();
+   
 }
